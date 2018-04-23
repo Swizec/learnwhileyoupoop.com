7e8e5f74426d7a9f3c1e7e6d7dbf3ccdbe6de869
Swizec Teller <swizec@swizec.com>
2018-04-22 18:33:42 -0700

    Read video list from youtube

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,80 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
- // You can delete this file if you're not using it
+const crypto = require('crypto')
+const ypi = require('youtube-playlist-info')
+const YT_KEY = require('./client_secrets.json')['yt_key']
+const LWyP = 'PLF8WgaD4xmjWuh7FTYTealxehOuNor_2S'
+
+exports.sourceNodes = async ({
+  boundActionCreators,
+  getNode,
+  hasNodeChanged,
+}) => {
+  const { createNode } = boundActionCreators
+  const makeNode = node => {
+    node.internal.contentDigest = crypto
+      .createHash('md5')
+      .update(JSON.stringify(node))
+      .digest('hex')
+
+    createNode(node)
+  }
+
+  const items = await ypi(YT_KEY, LWyP)
+
+  let ytNode = {
+    id: 'youtube',
+    children: ['ytPlaylists'],
+    parent: null,
+    internal: {
+      type: 'allYoutube',
+    },
+  }
+
+  let playlistsNode = {
+    id: 'ytPlaylists',
+    parent: 'youtube',
+    children: ['lwypPlaylist'],
+    internal: {
+      type: 'allYtPlaylists',
+    },
+  }
+
+  let lwypNode = {
+    id: 'lwypPlaylist',
+    parent: 'ytPlaylists',
+    children: [],
+    internal: {
+      type: 'ytPlaylist',
+    },
+  }
+
+  lwypNode.children = items.map(
+    ({ title, description, resourceId, thumbnails, position }) => {
+      const id = `ytVideo-${resourceId.videoId}`
+      makeNode({
+        id,
+        title,
+        description,
+        thumbnails,
+        position,
+        resourceId,
+        internal: {
+          type: 'ytVideo',
+        },
+        parent: 'lwypPlaylist',
+        children: [],
+      })
+      return id
+    }
+  )
+
+  makeNode(lwypNode)
+  makeNode(playlistsNode)
+  makeNode(ytNode)
+
+  // makeNode(lwypNode)
+
+  return
+}
