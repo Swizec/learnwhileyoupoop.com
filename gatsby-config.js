d3c4edeeae0cbf113bd2c4f0326bef7d7f3abfd6
Swizec Teller <swizec@swizec.com>
2018-05-14 02:01:59 -0700

    Add analytics code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,26 @@
 module.exports = {
   siteMetadata: {
     title: 'Learn While You Poop',
-    description:
-      'No pressure learning at 2 minutes per day. Start with WHY React all the way to a React 16.3 master in a month.',
+    description: 'No pressure learning at 2 minutes per day. Start with WHY React all the way to a React 16.3 master in a month.',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: ['gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-facebook-analytics',
+      options: {
+        includeInDevelopment: false,
+        appId: '714190382013726',
+        debug: false,
+        language: 'en_US'
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-1464315-30',
+        anonymize: true,
+        head: false,
+        respectDNT: true
+      }
+    }
+  ]
 }
