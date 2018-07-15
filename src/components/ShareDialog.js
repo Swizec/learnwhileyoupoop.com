f29291e87b21d5b6a960725d9885ff10ca549ac8
Helder S Ribeiro <hsribei.pub@gmail.com>
2018-07-14 20:33:21 -0400

    Move ShareDialog into a component file

---
+++ src/components/ShareDialog.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import styled from 'styled-components'
+import Textarea from 'react-textarea-autosize'
+
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  TwitterIcon,
+  FacebookIcon,
+} from 'react-share'
+
+import { FluffySection } from '../components/Section'
+import { FormControl } from 'react-bootstrap'
+import { MiddleColumn } from '../components/Columns'
+
+import Signature from '../img/signature.gif'
+
+const CenteredDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export default class ShareDialog extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      sharePlaceholder: props.sharePlaceholder,
+    }
+  }
+
+  handleChange = event => {
+    this.setState({ sharePlaceholder: event.target.value })
+  }
+
+  render() {
+    return (
+      <FluffySection>
+        <MiddleColumn>
+          <p style={{ textAlign: 'center' }}>
+            Know someone who wants to learn React and its whole ecosystem? Share
+            👇
+          </p>
+          <CenteredDiv>
+            <FormControl
+              componentClass={Textarea}
+              style={{
+                textAlign: 'center',
+                width: '500px',
+                fontSize: '1.5rem',
+              }}
+              value={this.state.sharePlaceholder}
+              onChange={this.handleChange}
+            />
+          </CenteredDiv>
+          <br />
+          <CenteredDiv>
+            <TwitterShareButton
+              via="swizec"
+              title={this.state.sharePlaceholder}
+              url={this.props.url}
+            >
+              <TwitterIcon size={32} round={true} />
+            </TwitterShareButton>
+            <FacebookShareButton url={this.props.url}>
+              <FacebookIcon size={32} round={true} />
+            </FacebookShareButton>
+          </CenteredDiv>
+          <p>
+            Cheers,<br />
+            ~Swizec<br />
+            <img
+              src={Signature}
+              style={{ width: '200px', margin: '1.5em 0' }}
+            />
+          </p>
+        </MiddleColumn>
+      </FluffySection>
+    )
+  }
+}
--- src/templates/article.js
+++ src/templates/article.js
@@ -3,88 +3,11 @@ import React from 'react'
 import Layout from '../components/layout'
 import ResponsivePlayer from '../components/ResponsivePlayer'
 import SocialHelmet from '../components/SocialHelmet.js'
+import ShareDialog from '../components/ShareDialog.js'
 
+import { LowSection } from '../components/Section'
 import Pagination from '../components/Pagination'
-import { FluffySection, LowSection } from '../components/Section'
-import { FormControl, Row, Col as Column } from 'react-bootstrap'
-
-import {
-  FacebookShareButton,
-  TwitterShareButton,
-  TwitterIcon,
-  FacebookIcon,
-} from 'react-share'
-import Signature from '../img/signature.gif'
-import { MiddleColumn } from '../components/Columns'
-
-import styled from 'styled-components'
-import Textarea from 'react-textarea-autosize'
-
-const CenteredDiv = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
-
-class ShareDialog extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      sharePlaceholder: props.sharePlaceholder,
-    }
-  }
-
-  handleChange = event => {
-    this.setState({ sharePlaceholder: event.target.value })
-  }
-
-  render() {
-    return (
-      <FluffySection>
-        <MiddleColumn>
-          <p style={{ textAlign: 'center' }}>
-            Know someone who wants to learn React and its whole ecosystem? Share
-            👇
-          </p>
-          <CenteredDiv>
-            <FormControl
-              componentClass={Textarea}
-              style={{
-                textAlign: 'center',
-                width: '500px',
-                minHeight: '8rem',
-                fontSize: '1.5rem',
-              }}
-              value={this.state.sharePlaceholder}
-              onChange={this.handleChange}
-            />
-          </CenteredDiv>
-          <br />
-          <CenteredDiv>
-            <TwitterShareButton
-              via="swizec"
-              title={this.state.sharePlaceholder}
-              url={this.props.url}
-            >
-              <TwitterIcon size={32} round={true} />
-            </TwitterShareButton>
-            <FacebookShareButton url={this.props.url}>
-              <FacebookIcon size={32} round={true} />
-            </FacebookShareButton>
-          </CenteredDiv>
-          <p>
-            Cheers,<br />
-            ~Swizec<br />
-            <img
-              src={Signature}
-              style={{ width: '200px', margin: '1.5em 0' }}
-            />
-          </p>
-        </MiddleColumn>
-      </FluffySection>
-    )
-  }
-}
+import { Row, Col as Column } from 'react-bootstrap'
 
 export default ({ data, pathContext }) => {
   const article = data.markdownRemark
