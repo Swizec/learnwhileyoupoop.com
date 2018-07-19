b624be11185aa9f62e2e8f8eadac639d9a107f73
Helder S Ribeiro <hsribei.pub@gmail.com>
2018-07-19 01:04:38 -0400

    Link to articles from titles on home page

--- src/components/CurriculumLink.js
+++ src/components/CurriculumLink.js
@@ -2,19 +2,13 @@ import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'gatsby'
 
-const ClickableH3 = styled.h3`
-  cursor: pointer;
-`
-
-const Description = styled.div`
-  width: 720px;
-`
-
-const CenteredDesc = Description.extend`
-  text-align: center;
-
-  @media (max-width: 720px) {
-    width: 95%;
+const TitleLink = styled(Link)`
+  &:link,
+  &:visited,
+  &:hover,
+  &:active {
+    border-bottom-style: none;
+    color: inherit;
   }
 `
 
@@ -23,16 +17,12 @@ class CurriculumLink extends React.Component {
     const { video, article } = this.props
 
     return (
-      <div>
-        <ClickableH3 onClick={this.onClick}>
-          {'👉'}{' '}
-          {article && article.fields && article.fields.slug ? (
-            <Link to={article.fields.slug}>{video.title.split('|')[0]}</Link>
-          ) : (
-            video.title.split('|')[0]
-          )}
-        </ClickableH3>
-      </div>
+      <h3>
+        {'👉'}{' '}
+        <TitleLink to={article.fields.slug}>
+          {video.title.split('|')[0]}
+        </TitleLink>
+      </h3>
     )
   }
 }
